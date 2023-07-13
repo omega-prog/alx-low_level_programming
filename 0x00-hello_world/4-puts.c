cd72fe2a695627fa745e36b0618ffa11da136f98
Fady <fadyn125@gmail.com>
2023-07-13 15:59:09 -0700

    First Print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* Printing sentence without using printf */
 int main(void)
 	{
-		puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 	}
