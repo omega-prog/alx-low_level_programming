efcdf913bf3210a2c5cda9c35c3c2a20a40e7e06
Fady <fadyn125@gmail.com>
2023-07-17 17:59:23 -0700

    Modify

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
-
-[200~#include <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     */
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-		int a;
+	int a;
 
-			for (a = 0; a < 10; a++)
-						printf("%i", a);
-				printf("\n");
-					return (0);
+	for (a = 0; a < 10; a++)
+		printf("%i", a);
+	printf("\n");
+	return (0);
 }
