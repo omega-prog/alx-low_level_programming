e99b31448a2bd27db66ac18fe00a2fb985cbd974
Fady <fadyn125@gmail.com>
2023-08-14 13:23:39 -0700

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//Main function returns combination of unique 2 digits.
+int main(void)
+{
+    int i = 0;
+
+    while (i++ < 89)
+    {
+        if (i / 10 != i % 10 && i / 10 < i % 10)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+
+            if (i == 89)
+            break;
+
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    //Return 0 if sucess.
+    return (0);
+}
