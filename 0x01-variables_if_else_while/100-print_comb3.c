c92acc6ac7ae292ad9a522878f5f337c09a5258b
Fady <fadyn125@gmail.com>
2023-08-14 13:42:26 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
-/*
- * Main function returns combination of unique 2 digits.
- **/
+#include <stdio.j>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i++ < 89)
-    {
-        if (i / 10 != i % 10 && i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-
-            if (i == 89)
-            break;
-
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    /*
-     * Return 0 if sucess.
-     **/
-    return (0);
+	while (i++ < 89)
+	{
+	if (i / 10 != i % 10 && i / 10 < i % 10)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		if (i == 89)
+		break;
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
 }
