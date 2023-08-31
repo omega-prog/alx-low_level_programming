164ab96f8cfac17100dd9bdb968673c33a1a10b7
Fady <fadyn125@gmail.com>
2023-08-31 11:56:39 -0700

    Last Number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - function prints the multiplication table.
+ * @n: The number would be provided by the user.
+ * no return here as the function is void.
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return (0);
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			product = x * y;
+
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				if (product < 100)
+					_putchar(' ');
+			}
+			if (product >= 100)
+			{
+				_putchar((product / 100) + '0');
+				product %= 100;
+				_putchar((product / 10) + '0');
+				product %= 10;
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				product %= 10;
+			}
+			_putchar(product + '0');
+		}
+		_putchar('\n');
+	}
+}
