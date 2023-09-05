029f53fe97e29fe3c498fadabad208b378ad182b
Fady <fadyn125@gmail.com>
2023-09-05 04:37:20 -0700

    Finished projects

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+
+/**
+ * main - Entry point the prints the sum of all natural numbers
+ * that are devisible by 3 and 5.
+ * Return: 0 while success.
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		sum += i;
+	}
+	printf("The sum is %d\n", sum);
+	return (0);
+}
