cf6e2feb3191add76e4e277b7aac383a0110a808
Fady <fadyn125@gmail.com>
2023-09-05 10:48:34 -0700

    Finished projects

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
-
 /**
- * main- Entry point.
- * Return: 0 while success.
- */
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
+*/
 int main(void)
 {
-	unsigned long int a = 1, b = 2, temp;
-	int i;
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
 
-	printf("%lu, ", a);
-	for (i = 0; i < 97; i++)
-	{
-		printf("%lu", b);
-		if (i < 96)
-			printf(", ");
-		else
-			printf("\n");
-		temp = a + b;
-		a = b;
-		b = temp;
-	}
-	return (0);
+for (inc = 0; inc < 92; inc++)
+{
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
+}
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
+{
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
+
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
+}
+printf("\n");
+return (0);
 }
