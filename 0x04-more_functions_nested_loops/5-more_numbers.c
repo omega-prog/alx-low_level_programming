1040571de8813383b5d5e1feb1bd87279193bda8
Fady <fadyn125@gmail.com>
2023-09-06 11:41:46 -0700

    Finishing projects

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n <= 9)
-				_putchar(n + '0');
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
+			if (n > 9)
+				_putchar('1');
+			_putchar((n % 10) + '0');
 		}
 	}
+	_putchar('\n');
 
 }
