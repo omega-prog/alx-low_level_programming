927ec8bcaed66f3b205b752a18f4d7fe464e7744
Fady <fadyn125@gmail.com>
2023-09-06 09:34:02 -0700

    Finishing projects

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * mul- Multiplies 2 numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: total.
+ */
 int mul(int a, int b)
 {
 	int total;
 
 	total = a * b;
 
-	return (0);
+	return (total);
 }
