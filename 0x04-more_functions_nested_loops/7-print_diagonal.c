ba9ac0453f7318dedbbce33dabf78f17d99e2ea6
Fady <fadyn125@gmail.com>
2023-09-06 12:28:25 -0700

    Finishing projects

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,13 @@ void print_diagonal(int n)
 {
 	int i, s;
 
+	if (n <= 0)
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (s = 0; s < i; s++)
 			_putchar(' ');
-		_putchar(92);
-		_putchar(32);
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
