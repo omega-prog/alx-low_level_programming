de760002a677f3ed808d2410450fd1e621e45922
Fady <fadyn125@gmail.com>
2023-09-06 12:19:49 -0700

    Finishing projects

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal- Prints diagonal line.
+ * @n: Height of the diagonal line.
+ */
+void print_diagonal(int n)
+{
+	int i, s;
+
+	for (i = 0; i < n; i++)
+	{
+		for (s = 0; s < i; s++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
